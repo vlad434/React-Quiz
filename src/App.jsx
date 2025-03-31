7f2da04ec1bc3630853c1242d6ce3752a1990cf6
Ilie Vlad <32937795+vlad434@users.noreply.github.com>
2025-03-31 15:13:02 +0300

    added external library, json-server. Successfully fetched mock data and make use of useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import MainSection from "./MainSection";
 
+const initialState = {
+  questions: [],
+  // 'loading' , 'error' , 'ready' , 'active' , 'finished'
+  status: "loading",
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("Action unknown");
+  }
+};
+
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const { questions, status } = state;
+
+  useEffect(() => {
+    fetch("http://localhost:8000/questions")
+      .then((response) => response.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch(() => dispatch({ type: "dataFailed" }));
+  });
+
   return (
     <div className="app">
       <Header />
