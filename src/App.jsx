46b141b026996f9ba25e47e6b3ce1eee1d29213d
Ilie Vlad <32937795+vlad434@users.noreply.github.com>
2025-03-31 14:57:35 +0300

    created new comp. MainSection and added it to the UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,15 @@
+import Header from "./Header";
+import MainSection from "./MainSection";
+
 export default function App() {
-  return <div></div>;
+  return (
+    <div className="app">
+      <Header />
+
+      <MainSection>
+        <p>1/15</p>
+        <p>Question?</p>
+      </MainSection>
+    </div>
+  );
 }
